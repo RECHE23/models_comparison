915360c1bc1d0bef5ccea9d540ed9d049e41adbc
René Chenard <rene.chenard.1@ulaval.ca>
2020-06-21 06:31:33 -0400

    cleanup

---
+++ README.md
@@ -0,0 +1,51 @@
+# Models comparison tool &nbsp; [![Generic badge](https://img.shields.io/static/v1?style=social&logo=youtube&label=YouTube&message=Watch%20now!&url=www.youtube.com/c/symbolxchannel)](https://www.youtube.com/user/symbolxchannel/playlists)
+> René Chenard - June 2020
+
+Generates animated plots of different models of neural network classifiers during learning in order to compare them.
+
+| ![Tanh networks](/Amounts%20of%20neurons/amount_of_neurons_tanh_sequential.gif?raw=true "Tanh networks with different amounts of neurons per layer (Sequential, not shuffled).") |
+|:--:|
+| Figure 1 &nbsp;&mdash;&nbsp; Tanh networks with different amounts of neurons per layer (Sequential, not shuffled). |
+
+| ![Tanh networks](/Amounts%20of%20neurons/amount_of_neurons_tanh_batch.gif?raw=true "Tanh networks with different amounts of neurons per layer (Batches of 32, shuffled).") |
+|:--:|
+| Figure 2 &nbsp;&mdash;&nbsp; Tanh networks with different amounts of neurons per layer (Batches of 32, shuffled). |
+
+---
+
+### Content:
+
+Directory | Description | Links
+--- | --- | :---:
+Activation functions | Comparison of the effect of using different types of activation function. | &nbsp; [&#9312;](https://github.com/RECHE23/models_comparison/blob/master/Activation%20functions/activation_functions1.ipynb) &emsp; [&#9313;](https://github.com/RECHE23/models_comparison/blob/master/Activation%20functions/activation_functions2.ipynb) &nbsp;
+Amounts of layers | Comparison of the effect of using different depths of layers. | &nbsp; [&#9312;](https://github.com/RECHE23/models_comparison/blob/master/Amounts%20of%20layers/amount_of_layers_relu.ipynb) &emsp; [&#9313;](https://github.com/RECHE23/models_comparison/blob/master/Amounts%20of%20layers/amount_of_layers_tanh.ipynb) &nbsp;
+Amounts of neurons | Comparison of the effect of using different amounts of neurons per layer. | &nbsp; [&#9312;](https://github.com/RECHE23/models_comparison/blob/master/Amounts%20of%20neurons/amount_of_neurons_relu.ipynb) &emsp; [&#9313;](https://github.com/RECHE23/models_comparison/blob/master/Amounts%20of%20neurons/amount_of_neurons_tanh.ipynb) &nbsp;
+Learning rate | Comparison of the effect of using different learning rates. | &nbsp; [&#9312;](https://github.com/RECHE23/models_comparison/blob/master/Learning%20rate/learning_rate_relu.ipynb) &emsp; [&#9313;](https://github.com/RECHE23/models_comparison/blob/master/Learning%20rate/learning_rate_tanh.ipynb) &nbsp;
+Loss functions | Comparison of the effect of using different loss functions. | &nbsp; [&#9312;](https://github.com/RECHE23/models_comparison/blob/master/Loss%20functions/loss_functions_relu.ipynb) &emsp; [&#9313;](https://github.com/RECHE23/models_comparison/blob/master/Loss%20functions/loss_functions_tanh.ipynb) &nbsp;
+Optimizers | Comparison of the effect of using different optimizers. |  &nbsp; [&#9312;](https://github.com/RECHE23/models_comparison/blob/master/Optimizers/optimizers_relu1.ipynb) &emsp; [&#9313;](https://github.com/RECHE23/models_comparison/blob/master/Optimizers/optimizers_relu2.ipynb) &emsp; [&#9314;](https://github.com/RECHE23/models_comparison/blob/master/Optimizers/optimizers_tanh1.ipynb) &emsp; [&#9315;](https://github.com/RECHE23/models_comparison/blob/master/Optimizers/optimizers_tanh2.ipynb) &nbsp;
+Weight decay | Comparison of the effect of using different values of weight decay. | &nbsp; [&#9312;](https://github.com/RECHE23/models_comparison/blob/master/Weight%20decay/weight_decay_relu.ipynb) &emsp; [&#9313;](https://github.com/RECHE23/models_comparison/blob/master/Weight%20decay/weight_decay_tanh.ipynb) &nbsp;
+Weight initialization | Comparison of the effect of using different weight initialization methods. | &nbsp; [&#9312;](https://github.com/RECHE23/models_comparison/blob/master/Weight%20initialization/weight_initialization_relu.ipynb) &emsp; [&#9313;](https://github.com/RECHE23/models_comparison/blob/master/Weight%20initialization/weight_initialization_tanh.ipynb) &nbsp;
+
+---
+
+### Details:
+For simplicity reasons, these experiments are done on simple neural network classifiers with two classes. A negative output corresponds to one class while a positive output corresponds to the other class. The [error function](https://en.wikipedia.org/wiki/Error_function) is applied on the output neuron to ensure that the output is always between -1 and 1.
+
+Unless specified, the neural networks are composed of four layers (two hidden layers) with 32 neurons per layer and uses the [stochastic gradient descent optimizer](https://pytorch.org/docs/stable/optim.html#torch.optim.SGD) with a [learning rate](https://en.wikipedia.org/wiki/Learning_rate) of 0.01, a [momentum](https://en.wikipedia.org/wiki/Stochastic_gradient_descent#Momentum) of 0.9, [weight decay](https://en.wikipedia.org/wiki/Regularization_(mathematics)#Tikhonov_regularization) at 0.001. The default [loss function](https://en.wikipedia.org/wiki/Loss_functions_for_classification) is the [mean squared error](https://en.wikipedia.org/wiki/Mean_squared_error).
+
+The default sampling method is by shuffled batches of 32 samples.
+
+| :warning: CAUTION!      |
+| :--------------------------- |
+| **The results obtained by these experiments may vastly diverge with slightly different parameters.<br />Always verify what you infer from them!** |
+
+---
+
+### TO DO:
+- [x] Add the option to learn by various sampling methods (~~sequential~~, ~~batches~~, bootstrapping, ~~shuffling~~, etc).
+- [ ] Implement [dimensionality reduction](https://en.wikipedia.org/wiki/Dimensionality_reduction) (like [PCA](https://en.wikipedia.org/wiki/Principal_component_analysis), [UMAP](https://en.wikipedia.org/wiki/Nonlinear_dimensionality_reduction#Uniform_manifold_approximation_and_projection) or [tSNE](https://en.wikipedia.org/wiki/T-distributed_stochastic_neighbor_embedding)) to analyze higher dimensions datasets.
+- [ ] Display more information about the setup: loss function, architecture, optimizer.
+- [ ] Display more information about the learning: learning curve, weights & biases, metrics, etc.
+- [ ] Make a module or an application out of the Jupyter Notebook experiments.
+- [ ] Add different color themes.
+- [ ] Add support for 0-1 classifiers and multiple classes classifiers.
---
+++ utils.py
@@ -0,0 +1,270 @@
+# -*- coding: utf-8 -*-
+
+# Import Numpy:
+import numpy as np
+np.random.seed(42)
+
+# Import PyTorch:
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+torch.manual_seed(42)
+
+# Setting the device to use:
+device = None
+if torch.cuda.is_available():
+    torch.cuda.set_device(0)
+    device = torch.device('cuda:0')
+    print("Running on GPU 0")
+else:
+    print("Running on CPU")
+
+# Import MatPlotLib:
+import matplotlib
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as PathEffects
+from matplotlib.animation import FuncAnimation
+matplotlib.rcParams['figure.figsize'] = (10.0, 8.0)
+matplotlib.rcParams['animation.html'] = 'html5'
+matplotlib.rcParams["animation.writer"] = 'imagemagick'
+norm = cm.colors.Normalize(vmin=-1.0, vmax=1.0)
+plt.tight_layout()
+
+# Suppress useless warnings:
+import warnings
+warnings.filterwarnings("ignore")
+
+# Import SKLearn datasets:
+import sklearn.datasets
+
+# Import ffmpeg-python:
+import ffmpeg
+
+
+# Define the accuracy function:
+def accuracy(y_pred, y):
+    pred_y_copy = y_pred.clone()
+    pred_y_copy[pred_y_copy < 0] = -1
+    pred_y_copy[pred_y_copy >= 0] = 1
+    acc_pred = (pred_y_copy == y).float().sum() / y.shape[0]
+    return acc_pred * 100
+
+
+# Define the Hinge loss function:
+def hinge_loss(input, target, reduction='mean'):
+    if not (target.size() == input.size()):
+        warnings.warn("Using a target size ({}) that is different to the input size ({}). "
+                      "This will likely lead to incorrect results due to broadcasting. "
+                      "Please ensure they have the same size.".format(target.size(),
+                                                                      input.size()),
+                      stacklevel=2)
+    ret = 1 - input * target
+    if reduction != 'none':
+        ret = torch.mean(ret) if reduction == 'mean' else torch.sum(ret)
+    return ret
+
+
+# Define the Hinge loss Module:
+class HingeLoss(nn.Module):
+    __constants__ = ['reduction']
+
+    def __init__(self, reduction: str = 'mean') -> None:
+        self.reduction = reduction
+        super(HingeLoss, self).__init__()
+
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        return hinge_loss(input, target, reduction=self.reduction)
+
+
+# Custom Dataset class:
+class MyDataset(Dataset):
+    def __init__(self, ds):
+        self.X, self.y = ds
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, index):
+        return [self.X[index], self.y[index]]
+
+
+def convert_to_pytorch_dataset(X, y):
+    X_tensor = torch.from_numpy(X).float()
+    y_tensor = torch.from_numpy(y.reshape(-1, 1)).float()
+    X_tensor = X_tensor.to(device)
+    y_tensor = y_tensor.to(device)
+    return MyDataset((X_tensor, y_tensor))
+
+
+# Define the function that generates the animated gif:
+def generate_animated_plot(epochs=400, datasets=None, models=None,
+                           optimizers=None, criterion=None,
+                           filename="animation", batch_size=32, shuffle=True):
+    # Define the plotting function:
+    def plot_decision_boundaries(fig):
+        fig.clf()
+        h = .02  # Step size in the mesh.
+        i = 1
+
+        # Plot the points:
+        def plot_points(ax, X, y):
+            plt.scatter(X[:, 0], X[:, 1], s=40,
+                        c=y.flatten(), edgecolors='k', alpha=0.75)
+            ax.set_xlim(xx.min(), xx.max())
+            ax.set_ylim(yy.min(), yy.max())
+            ax.set_xticks(())
+            ax.set_yticks(())
+
+        # Iterate over datasets:
+        for ds_index, ds in enumerate(datasets):
+            # Separates the characteristics from the labels:
+            X, y = ds.X, ds.y
+
+            # Creates a mesh grid from the characteristics space:
+            x_span = np.linspace(min(X[:, 0]), max(X[:, 0]))
+            y_span = np.linspace(min(X[:, 1]), max(X[:, 1]))
+            xx, yy = np.meshgrid(x_span, y_span)
+            grid = torch.Tensor(np.c_[xx.ravel(), yy.ravel()])
+
+            # Initialize the subplot:
+            ax = fig.add_subplot(len(datasets), len(models[ds_index]) + 1, i)
+
+            # Adds a title:
+            if ds_index == 0:
+                ax.set_title("Input data", fontdict={'fontsize': 16,
+                                                     'fontweight': 'medium'})
+
+            # Plot the points:
+            plot_points(ax, X, y)
+            i += 1
+
+            # Iterate over models:
+            for model_name, model in models[ds_index].items():
+                ax = plt.subplot(len(datasets), len(models[ds_index]) + 1, i)
+
+                # Evaluate the accuracy:
+                y_pred = model.forward(X)
+                score = accuracy(y_pred, y)
+
+                # Evaluate the loss:
+                if isinstance(criterion, dict):
+                    loss = criterion[model_name](y_pred, y)
+                else:
+                    loss = criterion(y_pred, y)
+
+                # Plot the decision boundary:
+                pred_func = model.forward(grid)
+                z = pred_func.view(xx.shape).detach().numpy()
+
+                # Put the result into a color plot:
+                ax.contourf(xx, yy, z, zorder=0, vmin=-1.0, vmax=1.0,
+                            norm=norm, alpha=.95)
+                plt.contour(xx, yy, z, levels=[-.0001, 0.0001], colors='r')
+
+                # Plot the points:
+                plot_points(ax, X, y)
+
+                # Adds a title:
+                if ds_index == 0:
+                    ax.set_title(model_name, fontdict={'fontsize': 16,
+                                                       'fontweight': 'medium'})
+
+                # Adds the score:
+                txt1 = ax.text(0.04, 0.03, ('Loss: %.2f' % loss).lstrip('0'),
+                               horizontalalignment='left',
+                               verticalalignment='bottom',
+                               fontsize=14, color='w', weight='bold',
+                               transform=ax.transAxes)
+                txt2 = ax.text(0.96, 0.03, ('Acc: %.2f' % score).lstrip('0'),
+                               horizontalalignment='right',
+                               verticalalignment='bottom',
+                               fontsize=14, color='w', weight='bold',
+                               transform=ax.transAxes)
+                txt1.set_path_effects(
+                    [PathEffects.withStroke(linewidth=1, foreground='k')])
+                txt2.set_path_effects(
+                    [PathEffects.withStroke(linewidth=1, foreground='k')])
+                i += 1
+
+    # Define the update function:
+    def update(i):
+        label = f'Epoch {i} / {epochs}'
+        display_progress_bar(i, epochs,
+                             prefix='Working:   ', suffix='', length=90)
+
+        # Iterate over datasets:
+        for ds_index, ds in enumerate(datasets):
+            train_loader = DataLoader(ds, batch_size=batch_size, shuffle=shuffle)
+            for X_batch, y_batch in train_loader:
+                X_batch = X_batch.to(device)
+                y_batch = y_batch.to(device)
+                # Iterate over models:
+                for model_name, model in models[ds_index].items():
+                    def closure():
+                        # Do a forward pass:
+                        y_pred = model.forward(X_batch)
+
+                        # Evaluate the loss:
+                        if isinstance(criterion, dict):
+                            loss = criterion[model_name](y_pred, y_batch)
+                        else:
+                            loss = criterion(y_pred, y_batch)
+
+                        # Do the backward pass:
+                        optimizers[ds_index][model_name].zero_grad()
+                        loss.backward()
+                        return loss
+
+                    optimizers[ds_index][model_name].step(closure)
+
+        # Update the plot:
+        plot_decision_boundaries(fig)
+        fig.tight_layout(pad=5.00)
+        fig.subplots_adjust(bottom=0.1, top=0.9)
+
+        # Update the title:
+        fig.suptitle(label, fontsize=25, y=0.075, x=0.978, ha='right')
+        if i == epochs:
+            print()
+            print(u'\x1b[?25l', end='')
+            print("Generating the animated gif... Please wait!", end=u"\r")
+
+    fig = plt.figure(figsize=(32, 18), dpi=80)  # WQHD Resolution: 2560 x 1440
+    anim = FuncAnimation(fig, update, frames=(epochs + 1), interval=1)
+    anim.save(f'{filename}.gif', dpi=80, writer='imagemagick')
+    print(u'\r' + ' ' * 90, end=u'\r')
+    print(u'\x1b[?25h', end='')
+
+
+def convert_to_video(filename, overwrite=True, options=None):
+    if options is None:
+        options = {'movflags': 'faststart', 'pix_fmt': 'yuv420p'}
+    stream = ffmpeg.input(f'{filename}.gif')
+    stream = ffmpeg.output(stream, f'{filename}.mp4', **options)
+    ffmpeg.run(stream, overwrite_output=overwrite)
+
+
+def compress_gif(filename, scale=1.0):
+    import subprocess
+    subprocess.call(["gifsicle", "--verbose", f'{filename}.gif',
+                     "--colors", "64",
+                     "--scale", f"{scale}",
+                     "--output", f'{filename}_compressed.gif',
+                     "-O3"])
+
+
+def display_progress_bar(iteration, total, prefix='', suffix='', decimals=1,
+                         length=100, fill=u'\u2588', print_end=u"\r"):
+    if iteration == 0:
+        print(u'\x1b[?25l', end='')
+    percent = ("{0:." + str(decimals) + "f}").format(
+        100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + '-' * (length - filled_length)
+    line = u'\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix)
+    print(line, end=print_end)
+    if iteration == total:
+        print(u'\r' + ' ' * len(line), end=u'\r')
+        print(u'\x1b[?25h', end='')
