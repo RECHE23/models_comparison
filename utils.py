cd45e0b15191784c45a638344da7db8ca7915952
René Chenard <rene.chenard.1@ulaval.ca>
2020-06-22 13:45:19 -0400

    Minor changes.

--- utils.py
+++ utils.py
@@ -67,6 +67,7 @@ def hinge_loss(input, target, reduction='mean'):
                                                                       input.size()),
                       stacklevel=2)
     ret = 1 - input * target
+    ret[ret < 0] = 0
     if reduction != 'none':
         ret = torch.mean(ret) if reduction == 'mean' else torch.sum(ret)
     return ret
@@ -82,6 +83,32 @@ class HingeLoss(nn.Module):
 
     def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         return hinge_loss(input, target, reduction=self.reduction)
+
+
+# Define the Linear loss function:
+def linear_loss(input, target, reduction='mean'):
+    if not (target.size() == input.size()):
+        warnings.warn("Using a target size ({}) that is different to the input size ({}). "
+                      "This will likely lead to incorrect results due to broadcasting. "
+                      "Please ensure they have the same size.".format(target.size(),
+                                                                      input.size()),
+                      stacklevel=2)
+    ret = (1 - input * target) / 2
+    if reduction != 'none':
+        ret = torch.mean(ret) if reduction == 'mean' else torch.sum(ret)
+    return ret
+
+
+# Define the Hinge loss Module:
+class LinearLoss(nn.Module):
+    __constants__ = ['reduction']
+
+    def __init__(self, reduction: str = 'mean') -> None:
+        self.reduction = reduction
+        super(LinearLoss, self).__init__()
+
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        return linear_loss(input, target, reduction=self.reduction)
 
 
 # Custom Dataset class:
